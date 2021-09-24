36033713778a92221128b676856f91f7eaff4389
mike12699 <335565@colum.edu>
2021-09-23 23:18:53 -0500

    Adds the CSV logic and results as a text file
    Results for CSV are now saved to a text file; it is a sample so far without mean and sum.

---
+++ GS_CSV/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GS_CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            addRecord("124", "Mercy", "56", "results.txt");
+        }
+
+        public static void addRecord(string ID, string Name, string Age, string filepath)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
+                {
+                    file.WriteLine(ID + "," + Name + "," + Age);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Bad program", ex);
+            }
+        }
+    }
+}
